6e5e8644b5db4ddbef7fa99024d8e1451105a089
Imperi13 <imperi.night@gmail.com>
2020-07-24 23:04:38 +0900

    ログイン画面へのリダイレクトを実装

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/Home.vue";
 import Axios from "../views/Axios.vue";
 import Login from "../views/Login.vue";
 import City from "../views/City.vue";
+import axios from "axios";
 
 Vue.use(VueRouter);
 
@@ -11,22 +12,34 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      isPublic: true
+    }
   },
   {
     path: "/axios",
     name: "axios",
-    component: Axios
+    component: Axios,
+    meta: {
+      isPublic: true
+    }
   },
   {
     path: "/login",
     name: "login",
-    component: Login
+    component: Login,
+    meta: {
+      isPublic: true
+    }
   },
   {
     path: "/city/:cityName",
     name: "City",
-    component: City
+    component: City,
+    meta: {
+      isPublic: false
+    }
   },
   {
     path: "/about",
@@ -35,12 +48,28 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+
+    meta: {
+      isPublic: true
+    }
   }
 ];
 
 const router = new VueRouter({
   routes
+});
+
+router.beforeEach(async (to, from, next) => {
+  try {
+    await axios.get("/api/whoami");
+  } catch (_) {
+    if (to.meta.isPublic) {
+      return next(true);
+    }
+    return next("/login");
+  }
+  next(true);
 });
 
 export default router;
